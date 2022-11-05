c2723e493f82c5f23dddc1c2768230e5be207389
Ivaylo <ivomoreras@abv.bg>
2022-11-05 16:31:19 +0200

    login form done

--- login-form/src/App.js
+++ login-form/src/App.js
@@ -2,8 +2,28 @@ import './App.css';
 
 function App() {
   return(
-    <div>
-      <h1> Hello World !</h1>
+    <div className='app'>
+      <div className='form'>
+        <h2>Sign in</h2>
+        <div className='input_box'>
+          <input type="text" required = "required" />
+          <span>Username</span>
+          <i></i>
+        </div>
+
+        <div className='input_box'>
+          <input type="password" required="required" pattern='[A-Za-z0-9]{1,20}'/>
+          <span>Password</span>
+          <i></i>
+        </div>
+
+        <div className='links'>
+          <a href="#">Forgot Password</a>
+          <a href="#">Signup</a>
+        </div>
+
+        <input type="submit" value = "Login"/>
+      </div>
     </div>
   )
 }
